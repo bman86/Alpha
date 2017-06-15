ca9b908f4b78355f00e6984e4ed4818f0bba664d
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-14 19:14:19 -0500

    Did migration for jobs table
      $ bin/rails db:migrate

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -69,6 +69,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(: title, :description, :salary)
+      params.require(:job).permit(:title, :description, :salary)
     end
 end
